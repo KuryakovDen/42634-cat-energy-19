04d1497623265e4fb9e846cc4ffed70a0d3c0bcd
Fifer2 <fifer2@yandex.ru>
2020-05-11 20:32:07 +0700

    fix: writing for 'toggle' correctly (B29)

--- source/js/app.js
+++ source/js/app.js
@@ -1,18 +1,18 @@
-const toogleButton = document.querySelector('.page-header__toogle');
+const toggleButton = document.querySelector('.page-header__toggle');
 const menu = document.querySelector('.page-header__navigation');
 
-toogleButton.classList.remove('page-header__toogle--no-js');
+toggleButton.classList.remove('page-header__toggle--no-js');
 
-toogleButton.addEventListener('click', function () {
-  if (toogleButton.classList.contains('page-header__toogle--opened')) {
-    toogleButton.classList.remove('page-header__toogle--opened');
-    toogleButton.classList.add('page-header__toogle--closed');
+toggleButton.addEventListener('click', function () {
+  if (toggleButton.classList.contains('page-header__toggle--opened')) {
+    toggleButton.classList.remove('page-header__toggle--opened');
+    toggleButton.classList.add('page-header__toggle--closed');
 
     menu.classList.add('page-header__navigation--closed');
     menu.classList.remove('page-header__navigation--opened');
   } else {
-    toogleButton.classList.add('page-header__toogle--opened');
-    toogleButton.classList.remove('page-header__toogle--closed');
+    toggleButton.classList.add('page-header__toggle--opened');
+    toggleButton.classList.remove('page-header__toggle--closed');
 
     menu.classList.add('page-header__navigation--opened');
     menu.classList.remove('page-header__navigation--closed');
