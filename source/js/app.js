20c8a9ae52cd7605b22797777587859d5688b046
Fifer2 <fifer2@yandex.ru>
2020-05-12 07:57:57 +0700

    fix: fix bug with showing/hiding header-menu

--- source/js/app.js
+++ source/js/app.js
@@ -2,6 +2,8 @@ const toggleButton = document.querySelector('.page-header__toggle');
 const menu = document.querySelector('.page-header__navigation');
 
 toggleButton.classList.remove('page-header__toggle--no-js');
+menu.classList.remove('page-header__navigation--opened');
+menu.classList.add('page-header__navigation--closed');
 
 toggleButton.addEventListener('click', function () {
   if (toggleButton.classList.contains('page-header__toggle--opened')) {
