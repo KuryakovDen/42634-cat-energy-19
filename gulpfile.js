4ba0fe80ac47382a6624f8ae50618e672f72b99d
Fifer2 <fifer2@yandex.ru>
2020-05-12 21:03:05 +0700

    feat: added webp-images (B13)

--- gulpfile.js
+++ gulpfile.js
@@ -78,7 +78,7 @@ gulp.task("images", function () {
 gulp.task("webp", function () {
   return gulp.src("source/img/**/*.{png,jpg}")
   .pipe(webp({quality: 90}))
-  .pipe(gulp.dest("source/css"))
+  .pipe(gulp.dest("source/img"))
 });
 
 gulp.task("copy", function () {
